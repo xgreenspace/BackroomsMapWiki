846d383b3f9b0c5c731121bf4adc819eacc4a8c4
Amariwest19 <Amariwest19@tamu.edu>
2022-12-07 04:10:19 -0600

    fixed bug in scraper where duplicate paragraphs were recieved

--- Backend/scrapeWikidot.py
+++ Backend/scrapeWikidot.py
@@ -7,6 +7,8 @@ import pandas as pd
 import re
 from urllib.request import urlopen
 
+from tqdm import tqdm
+
 normal_levels_url = "http://backrooms-wiki.wikidot.com/normal-levels-i"
 normal_levels_page = urlopen(normal_levels_url)
 
@@ -49,26 +51,63 @@ normal_levels_names = [x for x in normal_levels_names if 'Level' in x]
 normal_levels_names = normal_levels_names[7:]
 normal_levels_names = [x.replace('(This article is under rewrite)', '') for x in normal_levels_names]
 
-normal_levels_href = ['/level-18']
+
+def getLevelNames(text):
+    # Get the paragraph under the strong tag labeled "Entrances"
+    _out = soup.find(['strong', 'span'], text=text).find_next('p')
+
+    # Find all Level links in the paragraph filtering all the other links
+    _out = _out.find_all('a', href=re.compile('level-'))
+    output = []
+    for n in _out:
+        output.append(n.text)
+        
+    # Remove duplicates
+    output = list(dict.fromkeys(output))
+
+    # Remove any strings that do not fit the regex pattern of "Level [number]"
+    output = [x for x in output if re.match(r'Level \d+', x)]
+    return output
+
+
 normal_levels_enter_exit = []
-for href in normal_levels_href:
-    url = "http://backrooms-wiki.wikidot.com" + href
-    page = urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
+for href in tqdm(normal_levels_href[0:5]):
+    try:
+        url = "http://backrooms-wiki.wikidot.com" + href
+        page = urlopen(url)
+        soup = BeautifulSoup(page, 'html.parser')
+    except:
+        continue
 
     try:
-        # Get the paragraph under the strong tag labeled "Entrances"
-        enterances = soup.find('strong', text='Entrances:').find_next('p').text
-        enterances = re.findall(r'Level \d+', enterances)
-
-        # Get the paragraph under the strong tag labeled "Exits"
-        exits = soup.find('strong', text='Exits:').find_next('p').text
-        exits = re.findall(r'Level \d+', exits)
+        entrances = getLevelNames('Entrances:')
     except:
-        enterances = []
-        exits = []
+        try:
+            entrances = getLevelNames('Entrances')
+        except:
+            entrances = []
+    
+    try:
+        exits = getLevelNames('Exits:')
+    except:
+        try:
+            exits = getLevelNames('Exits')
+        except:
+            exits = []
 
-    normal_levels_enter_exit.append({"Level" : href, "Enterances" : enterances, "Exits" : exits})
+    normal_levels_enter_exit.append({"Level" : href, "Entrances" : entrances, "Exits" : exits})
+
+
+# Print normal_levels_enter_exit in a nice format
+for level in normal_levels_enter_exit:
+    print(level['Level'])
+    print('Entrances:')
+    for entrance in level['Entrances']:
+        print(entrance)
+    print('Exits:')
+    for exit in level['Exits']:
+        print(exit)
+    print()
 
 
 
